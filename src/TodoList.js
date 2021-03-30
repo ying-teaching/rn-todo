3179b5ca2e3946d0e59641279e63eb515e7a43d1
Ying Liu <yingliu4203@gmail.com>
2021-03-29 21:40:55 -0700

    use functional components

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import Todo from './Todo';
+import Todo from "./Todo";
 
-class TodoList extends Component {
-  render() {
-    const { user } = this.props;
-    return (
-      <View>
-        {user.todos.map(todo => (
-          <Todo key={todo.id} todo={todo} />
-        ))}
-      </View>
-    );
-  }
-}
-
-export default TodoList
+export default ({ user }) => {
+  return (
+    <View>
+      {user.todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </View>
+  );
+};
