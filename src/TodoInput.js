08269501b0d478728a88d7c92a1aead0b2edd175
Ying Liu <yingliu4203@gmail.com>
2021-03-29 22:06:08 -0700

    add button

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,24 +1,35 @@
-import React from "react";
-import { TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button } from "react-native";
 import { Mutation } from "@apollo/react-components";
 
 import styles from "./styles";
 import { GET_USER, ADD_TODO } from "../constants";
 
 export default () => {
+  const [text, setText] = useState("");
   return (
     <Mutation
       mutation={ADD_TODO}
       refetchQueries={[{ query: GET_USER, variables: { userId: "me" } }]}
     >
       {(addTodo) => (
-        <TextInput
-          style={styles.textInput}
-          placeholder="What needs to be done?"
-          onSubmitEditing={({ nativeEvent: { text } }) =>
-            addTodo({ variables: { text } })
-          }
-        />
+        <View style={{ flexDirection: "row" }}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="What needs to be done?"
+            onChangeText={(text1) => setText(text1)}
+            value={text}
+          />
+          <Button
+            title="Add Todo"
+            onPress={() => {
+              if (text) {
+                addTodo({ variables: { text } });
+                setText("");
+              }
+            }}
+          />
+        </View>
       )}
     </Mutation>
   );
